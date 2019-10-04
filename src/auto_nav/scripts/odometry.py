931ad46e99bfa464b11af846ad23b652fc6de8f8
swapnil <swapnilmane1393@gmail.com>
2019-10-04 17:14:58 +0530

    new code for rosserial test

--- src/auto_nav/scripts/odometry.py
+++ src/auto_nav/scripts/odometry.py
@@ -8,21 +8,17 @@
 ###################################################################################################################
 
 import rospy
-# from std_msgs.msg import Float32MultiArray,Int16MultiArray
 from math import *
 import tf
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
-from auto_nav.msg import uart_ticks_msg, uart_velocity_msg
+from auto_nav.msg import ticks_msg
 
 #All in MKS
 wheel_dia  = 0.154                                                                   # wheel diameter in m
 ppr = 135                                                                   # encoders ticks per rotation
 bot_dia = 0.475
 temp_distance = 0.00
-linear_velocity_x = 0
-linear_velocity_y = 0
-angular_velocity = 0
 angle = 0
 ##################################################################################################################
 
@@ -44,27 +40,17 @@ class Queue:                                                                # qu
 def ticks_callback(ticks_array):
     queue_left.enqueue(ticks_array.motor_L)
     queue_right.enqueue(ticks_array.motor_R)
-
-def velocity_callback(vel_array):
-    global linear_velocity_x, linear_velocity_y, angular_velocity, angle
-    left_vel_linear = (vel_array.motor_L)*0.077                             # angular speed converted to linear speed
-    right_vel_linear = (vel_array.motor_R)*0.077
-    angular_velocity = (right_vel_linear - left_vel_linear)/2               # instantaneous angular velocity of the robot
-    linear_velocity_x = ((right_vel_linear + left_vel_linear)/2)*cos(angle) # instantaneous linear velocity of the robot
-    linear_velocity_y = ((right_vel_linear + left_vel_linear)/2)*sin(angle)
-
+    
 #####################################################################################################################
 if __name__ == '__main__':
-    global linear_velocity_x, linear_velocity_y, angular_velocity,angle
     x = 0
     y = 0
     theta = 0
     angle = 0
     queue_right = Queue()                                                   # queue objects for right and left encoders
     queue_left = Queue()
-    rospy.init_node('talker', anonymous=False)
-    rospy.Subscriber("ticks", uart_ticks_msg, ticks_callback)                # Subscribing to raw encoder tics and velocities
-    rospy.Subscriber("velocity", uart_velocity_msg, velocity_callback)
+    rospy.init_node('avitra_odometry', anonymous=False)
+    rospy.Subscriber("ticks", ticks_msg, ticks_callback)                # Subscribing to raw encoder tics and velocities
     odom_broadcaster = tf.TransformBroadcaster()
     laser_broadcaster = tf.TransformBroadcaster()
     rate = rospy.Rate(10)
