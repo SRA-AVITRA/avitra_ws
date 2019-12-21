64cb8522666d94343b90035fec7a6ba6bf3fa1e5
shanks-d <shshnk.sd@gmail.com>
2019-12-21 23:31:47 +0530

    Added auto-save for plots in rpm_tuning script

--- src/auto_nav/scripts/rpm_tuning.py
+++ src/auto_nav/scripts/rpm_tuning.py
@@ -16,11 +16,11 @@ from auto_nav.msg import tuning_msg
 from auto_nav.msg import velocity_msg
 from matplotlib import pyplot as plt
 
-Kp = 0.003
-Kd = 0.75
+Kp = 0.0075
+Kd = 0.0
 Ki = 0.0
-rpmL=[]
-rpmR=[]
+rpmL = []
+rpmR = []
 
 def publish_tuna():
     global Kp, Kp, Ki, tuna
@@ -41,38 +41,41 @@ def getKey():
 
 def print_rpm(rpm):
     global rpmL,rpmR
-    rpmL.append(rpm.motorL)
-    rpmR.append(rpm.motorR)
+    rpmL.append(rpm.motor_L)
+    rpmR.append(rpm.motor_R)
 
 def plot():
     global rpmL, rpmR
-    plt.plot(rpmL)
-    plt.plot(rpmR)
+    fig, axs = plt.subplots(2)
+    axs[0].plot(rpmL)
+    axs[1].plot(rpmR)
+    axs[0].set_title('MOTOR_L')
+    axs[1].set_title('MOTOR_R')
     plt.show()
-
-
+    title = "/home/shanks/tuning_plots/P/phd/"+str(Kp)+"_"+str(Kd)+"_"+str(Ki)+".png"
+    fig.savefig(title)
 
 if __name__=="__main__":
     rospy.init_node('rpm_tuning',anonymous=False)
     settings = termios.tcgetattr(sys.stdin)
     pub = rospy.Publisher('tuning', tuning_msg, queue_size=10)          # publisher for teleop_key
-    rospy.Subscriber("/rpm", velocity_msg , print_rpm)
+    rospy.Subscriber("rpm", velocity_msg , print_rpm)
     tuna = tuning_msg()
     while not rospy.is_shutdown():
         print "Kp =", Kp, "\tKd =", Kd, "\tKi =", Ki
         key = getKey()
         if key == "p":
-            Kp+=0.0005
+            Kp+=0.0001
         elif key == "l":
-            Kp-=0.0005
+            Kp-=0.0001
         elif key == "d":
-            Kd+=0.05
+            Kd+=0.001
         elif key == "x":
-            Kd-=0.05
+            Kd-=0.001
         elif key == "i":
-            Ki+=0.001
+            Ki+=0.0005
         elif key == "j":
-            Ki-=0.001
+            Ki-=0.0005
         elif key == "q":
             plot()
             exit()
