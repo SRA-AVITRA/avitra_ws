5d6046ad9b9d7ae65d32ff143ff3ae769c39de04
sm1393 <swapnilmane1393@gmail.com>
2020-01-16 14:51:32 +0530

    Added new codes for tuned PI controller with pid_response msgs

--- src/auto_nav/scripts/pwm_vs_rpm.py
+++ src/auto_nav/scripts/pwm_vs_rpm.py
@@ -5,6 +5,7 @@ import rospy
 import sys, select, termios, tty
 from auto_nav.msg import velocity_msg
 from matplotlib import pyplot as plt
+import matplotlib.animation as animation
 import xlwt 
 from xlwt import Workbook
 
@@ -16,16 +17,16 @@ sheet_R = 0
 def deploy_payloads(direction, speed):
     global motor_L, motor_R
     if direction == "w":
-        motor_L = speed
+        motor_L = 0
         motor_R = -speed
     elif direction == "s":
-        motor_L = -speed
+        motor_L = 0
         motor_R = speed
     elif direction == "a":
-        motor_L = -speed
+        motor_L = 0
         motor_R = -speed
     elif direction == "d":
-        motor_L = speed
+        motor_L = 0
         motor_R = speed
     else:
         motor_L = 0
@@ -45,28 +46,56 @@ def getKey():
     return key
 
 def rpm_callback(rpm):
-    rpm_L.append(rpm.motor_L)
     rpm_R.append(rpm.motor_R)
 
-def plot():
-    for i in range(len(rpm_L)):
-        global sheet_L, sheet_R
-        axs[0].scatter(rpm_L[i], pwm.motor_L)
-        sheet1.write(sheet_L, 0, rpm_L[i])
-        sheet1.write(sheet_L, 1, pwm.motor_L)
-        sheet_L += 1
+def save_records():
+    wb.save('/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/sheets/xls/rpm_vs_pwm_forward_pwm_'+str(speed)+'.xls') 
+    # fig.savefig('/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/pwm_vs_rpm.png')
+    plt.show()
+
+def animate(i):
+    # for i in range(len(rpm_L)):
+    #     global sheet_L, sheet_R
+    #     axs[0].scatter(rpm_L[i], pwm.motor_L)
+    #     sheet1.write(sheet_L, 0, rpm_L[i])
+    #     sheet1.write(sheet_L, 1, pwm.motor_L)
+    #     sheet_L += 1
     for i in range(len(rpm_R)):
         axs[1].scatter(rpm_R[i], pwm.motor_R)
         sheet1.write(sheet_R, 3, rpm_R[i])
         sheet1.write(sheet_R, 4, pwm.motor_R)
         sheet_R += 1
-    del rpm_L[:]
+    # del rpm_L[:]
     del rpm_R[:]
 
-def save_plot():
-    wb.save('/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/sheets/xls/rpm_vs_pwm_forward_pwm_'+str(speed)+'.xls') 
-    # fig.savefig('/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/pwm_vs_rpm.png')
-    plt.show()
+    print "direction =", direction, "\tspeed =", speed
+    key = getKey()
+    if key == "r":
+        direction = "r"
+    elif key == "w":
+        direction = "w"
+    elif key == "s":
+        direction = "s"
+    elif key == "a":
+        direction = "a"
+    elif key == "d":
+        direction = "d"
+    elif key == "y":
+        record()
+        speed += 1
+        if speed > 100:
+            speed = 100
+    elif key == "h":
+        record()
+        speed -= 1
+        if speed < 32:
+            speed = 32
+    elif key == "x":
+        record()
+        deploy_payloads("r", 0)
+        save_records()
+        exit()
+    deploy_payloads(direction, speed)
 
 
 if __name__=="__main__":
@@ -80,37 +109,17 @@ if __name__=="__main__":
     speed = 32
     motor_L = 0
     motor_R = 0
-    fig, axs = plt.subplots(2)
-    axs[0].set_title('MOTOR_L')
-    axs[1].set_title('MOTOR_R')
-    axs[0].set(xlabel='rpm_L', ylabel='pwm_L')
-    axs[1].set(xlabel='rpm_R', ylabel='pwm_R')
+    # fig, axs = plt.subplots(2)
+    # # axs[0].set_title('MOTOR_L')
+    # axs[1].set_title('MOTOR_R')
+    # axs[0].set(xlabel='rpm_L', ylabel='pwm_L')
+    # axs[1].set(xlabel='rpm_R', ylabel='pwm_R')
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+    ax1.set_title("rpm_R_vs_pwm_R")
+
     wb = Workbook()
     sheet1 = wb.add_sheet('rpm_vs_pwm')
-    while not rospy.is_shutdown():
-        print "direction =", direction, "\tspeed =", speed
-        key = getKey()
-        if key == "r":
-            plot()
-            direction = "r"
-        elif key == "w":
-            direction = "w"
-        elif key == "s":
-            direction = "s"
-        elif key == "a":
-            direction = "a"
-        elif key == "d":
-            direction = "d"
-        elif key == "y":
-            speed += 1
-            if speed > 100:
-                speed = 100
-        elif key == "h":
-            speed -= 1
-            if speed < 32:
-                speed = 32
-        elif key == "x":
-            deploy_payloads("r", 0)
-            save_plot()
-            exit()
-        deploy_payloads(direction, speed)
+    ani = animation.FuncAnimation(fig, animate, interval = 1)
+    fig.show()
--- src/auto_nav/scripts/rpm_tuning_off_load_live_plot.py
+++ src/auto_nav/scripts/rpm_tuning_off_load_live_plot.py
@@ -19,16 +19,32 @@ from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 
-i = 0
-Kp = 2.5
+# Kp = 2.5
+# Kd = 2000
+# Ki = 0.05
+
+# Kp = 3.5
+# Kd = 125
+# Ki = 0.25
+
+Kp = 2
 Kd = 0
-Ki = 0.4
-iTerm_max = 50
-iTerm_min = -50
-rpm_R = []
+Ki = 0.35
+pwm_frequency = 200
+
+curr_rpm_R = []
 duty_cycle_R = []
-samples = []
+err_R = []
+pTerm_R = []
+dTerm_R = []
+iTerm_R = []
+prev_err_R = []
+err_diff = []
+
+i = 0
+iTerm_limit = 0
 desr_rpm = 0
+alpha = 0
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -41,11 +57,19 @@ def getKey():
     return key
 
 def animate(i):
-    global rpm_R, desr_rpm, Kp, Kd, Ki
+    # if i not in range(0,2000):
+    #     print "done"
+    #     exit()
+    global rpm_R, desr_rpm, Kp, Kd, Ki, alpha
     if desr_rpm != 0:
         ax1.axhline(y=desr_rpm, color='b', linestyle='-')
-        ax1.plot(rpm_R, color = 'r')
+        ax1.plot(curr_rpm_R, color = 'r')
         ax2.plot(duty_cycle_R, color = 'r')
+        ax3.plot(err_R, color = 'r')
+        ax4.plot(pTerm_R, color = 'r')
+        ax5.plot(dTerm_R, color = 'r')
+        ax6.plot(iTerm_R, color = 'r')
+        # ax6.plot(err_diff, color = 'r')
     print "Kp =", Kp, "\t\tKd =", Kd, "\tKi =", Ki
     key = getKey()
     if key == "p":
@@ -73,16 +97,26 @@ def publish_tuna():
     pub.publish(tuna)
 
 def pid_callback(pid_response):
-    global rpm_R, desr_rpm, duty_cycle_R, i
-    rpm_R.append(pid_response.curr_rpm)
+    global rpm_R, desr_rpm, duty_cycle_R, i, alpha
+    
+    curr_rpm_R.append(pid_response.curr_rpm)
     duty_cycle_R.append(pid_response.duty_cycle)
+    err_R.append(pid_response.err)
+    pTerm_R.append(pid_response.pTerm)
+    dTerm_R.append(pid_response.dTerm)
+    iTerm_R.append(pid_response.iTerm)
+    iTerm_limit = pid_response.iTerm_limit
+    alpha = pid_response.alpha
+    # prev_err_R.append(pid_response.prev_err)
+    # err_diff.append(pid_response.err - pid_response.prev_err)
     desr_rpm = pid_response.desr_rpm
     i += 1
 
 def plot():
-    # title = "/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/rpm_tuning/off_load_right_motor/duty_is_pid_term/"+str(Kp)+"_"+str(Kd)+"_"+str(Ki)+"_"+str(iTerm_max)+"_"+str(iTerm_min)+".png"
-    title = "/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/rpm_tuning/off_load_right_motor/duty_is_pid_term/temp.png"
-    plt.savefig(title)
+    # title = "/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/rpm_tuning/off_load_right_motor/duty_is_pid_term_live/15_Jan_2020/Kp"+str(Kp)+"_Kd"+str(Kd)+"_Ki"+str(Ki)+"_pwm_freq"+str(pwm_frequency)+"_alpha"+str(alpha)+"_iTerm_limit"+str(iTerm_limit)+".png"
+    # title = "/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/rpm_tuning/off_load_right_motor/duty_is_pid_term/temp.png"
+    title = "/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/rpm_tuning/off_load_right_motor/duty_is_pid_term_live/15_Jan_2020/best_fit/2_0_0.35_samples2000_alpha"+str(alpha)+".png"
+    # plt.savefig(title)
     plt.show()
 
 if __name__=="__main__":
@@ -92,9 +126,25 @@ if __name__=="__main__":
     rospy.Subscriber("pid_response", pid_response , pid_callback)
     tuna = tuning_msg()    
     fig = plt.figure()
-    ax1 = fig.add_subplot(1,2,1)
-    ax2 = fig.add_subplot(1,2,2)
+    
+    # ax1 = fig.add_subplot(1,2,1)
+    # ax2 = fig.add_subplot(1,2,2)
+    # ax1.set_title('curr_rpm_R')
+    # ax2.set_title('duty_cycle_R')
+
+    ax1 = fig.add_subplot(2,3,1)
+    ax2 = fig.add_subplot(2,3,2)
+    ax3 = fig.add_subplot(2,3,3)
+    ax4 = fig.add_subplot(2,3,4)
+    ax5 = fig.add_subplot(2,3,5)
+    ax6 = fig.add_subplot(2,3,6)
+
     ax1.set_title('curr_rpm_R')
-    ax2.set_title('duty_cycle_R')
+    ax2.set_title('duty_cycle_R')    
+    ax3.set_title('err_R')    
+    ax4.set_title('pTerm_R')    
+    ax5.set_title('dTerm_R')    
+    ax6.set_title('iTerm')    
+    
     ani = animation.FuncAnimation(fig, animate, interval = 1)
     fig.show()        
