811cefd77e8e3952867434640e36d0ba6d47e173
shanks-d <shshnk.sd@gmail.com>
2020-01-17 11:00:42 +0530

    Added command in velocity.msg and ros_tran and Added autonav_test.py

---
+++ src/auto_nav/scripts/autonav_test.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+###################################################################################################################
+# CODE TO GIVE RPM VIA TWIST
+# PRESS 'y' TO INCREMENT LINEAR_X BY 5rpm
+# PRESS 'h' TO DECREMENT LINEAR_X BY 5rpm
+# PRESS 'i' TO INCREMENT ANGULAR_Z BY 5rpm
+# PRESS 'k' TO DECREMENT ANGULAR_Z BY 5rpm
+# PRESS 'x' TO EXIT
+###################################################################################################################
+
+import struct
+import rospy
+from std_msgs.msg import String
+import sys, select, termios, tty
+from geometry_msgs.msg import Twist
+
+bot_radius = 0.475/2
+wheel_radius = 0.154/2
+pi = 3.14159265358
+
+def publish(linear_x, angular_z):
+    global twist
+    linear_x *= 2 * pi * wheel_radius / 60 
+    angular_z *= 2 * pi * wheel_radius * bot_radius / 60 
+    twist.linear = [linear_x, 0, 0]
+    twist.angular = [0, 0, angular_z]
+    pub.publish(twist)
+    
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)                                         # read key pressed on keyboard
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+if __name__=="__main__":
+    rospy.init_node('autonav_test',anonymous=False)
+    settings = termios.tcgetattr(sys.stdin)
+    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)          # publisher for Twist data
+    twist = Twist()
+    x = 0
+    z = 0
+    publish(x, z)
+    while not rospy.is_shutdown():
+        print "x =", x, "\tz =", z, "\tlinear_x =", twist.linear.x, "\tangular_z =", twist.angular.z
+        key = getKey()
+        if key == "y":
+            x += 5
+        elif key == "h":
+            x -= 5
+        if key == "i":
+            z += 5
+        elif key == "k":
+            z -= 5
+        elif key == "r":
+            x = 0
+            z = 0
+        elif key == "x":
+            deploy_payloads(0, 0)
+            exit()
+        deploy_payloads(x, z)
--- src/auto_nav/scripts/ros_tran.py
+++ src/auto_nav/scripts/ros_tran.py
@@ -18,22 +18,34 @@ import sys, select, termios, tty
 from auto_nav.msg import velocity_msg
 
 def deploy_payloads(direction, speed):
-    global motor_L, motor_R, velocity
+    global motor_F, motor_B, motor_L, motor_R, velocity
     if direction == "w":
+        motor_F = 0
+        motor_B = 0
         motor_L = speed
         motor_R = -speed
     elif direction == "s":
+        motor_F = 0
+        motor_B = 0
         motor_L = -speed
         motor_R = speed
     elif direction == "a":
+        motor_F = -speed
+        motor_B = -speed
         motor_L = -speed
         motor_R = -speed
     elif direction == "d":
+        motor_F = speed
+        motor_B = speed
         motor_L = speed
         motor_R = speed
     else:
+        motor_F = 0
+        motor_B = 0
         motor_L = 0
         motor_R = 0
+    velocity.motor_F = motor_F
+    velocity.motor_B = motor_B
     velocity.motor_L = motor_L
     velocity.motor_R = motor_R
     pub_velocity.publish(velocity)
@@ -56,10 +68,12 @@ if __name__=="__main__":
     deploy_payloads("r", 0)
     direction = "r"
     speed = 0
+    motor_F = 0
+    motor_B = 0
     motor_L = 0
     motor_R = 0
     while not rospy.is_shutdown():
-        print "direction =", direction, "\tspeed =", speed
+        print "direction =", direction, "\tspeed =", speed, "\tcommand =", velocity.command
         key = getKey()
         if key == "r":
             direction = "r"
@@ -79,6 +93,10 @@ if __name__=="__main__":
             speed -= 1
             if speed < 0:
                 speed = 0
+        elif key == "q":
+            velocity.command = 0
+        elif key == "z":
+            velocity.command = 1
         elif key == "x":
             deploy_payloads("r", 0)
             exit()
