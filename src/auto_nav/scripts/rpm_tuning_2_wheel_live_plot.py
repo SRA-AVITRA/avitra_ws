688d10a925b2415840137283265ceaeb4c2b36a9
sm1393 <swapnilmane1393@gmail.com>
2020-01-18 10:32:40 +0530

    Added new code for on-load rpm tuning to calculate difference between their angular speeds

--- src/auto_nav/scripts/rpm_tuning_2_wheel_live_plot.py
+++ src/auto_nav/scripts/rpm_tuning_2_wheel_live_plot.py
@@ -40,12 +40,16 @@ from matplotlib import style
 # pwm_frequency = 200
 
 #on load 4 wheel
-Kp = 2
-Kd = 0
-Ki = 0.4
+Kp = 1.5
+Kd = 0.05
+Ki = 0.5
 
+i = 0
+desr_rpm = 50
 curr_rpm_R = []
 curr_rpm_L = []
+err = []
+flag = False
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -58,29 +62,18 @@ def getKey():
     return key
 
 def animate(j):
-    global curr_rpm_R, curr_rpm_L
+    global Kp, Kd, Ki, desr_rpm, curr_rpm_R, curr_rpm_L, i, err, flag
     if i not in range(0,2000):
         end()
-    if desr_rpm != 0:
+    if  flag == True:
         ax1.axhline(y=desr_rpm, color='b', linestyle='-')
         ax2.axhline(y=desr_rpm, color='b', linestyle='-')
         ax1.plot(curr_rpm_R, color = 'r')
         ax2.plot(curr_rpm_L, color = 'r')
+        ax3.plot(err, color = 'r')
     print "Kp =", Kp, "\tKd =", Kd, "\tKi =", Ki, "i = ", i
     key = getKey()
-    if key == "p":
-        Kp+=0.01
-    elif key == "l":
-        Kp-=0.01
-    elif key == "d":
-        Kd+=0.0001
-    elif key == "x":
-        Kd-=0.0001
-    elif key == "i":
-        Ki+=0.0005
-    elif key == "j":
-        Ki-=0.0005
-    elif key == "q":
+    if key == "q":
         end()
     publish_tuna()
 
@@ -92,20 +85,22 @@ def publish_tuna():
     pub.publish(tuna)
 
 def pid_callback(pid_response):
-    global curr_rpm_R, curr_rpm_L
-    desr_rpm = pid_response.desr_rpm
-    if pid_response.name = 'MOTOR_L'
-        curr_rpm_L.append(pid_response.curr_rpm)
-    elif pid_response.name = 'MOTOR_R'
-        curr_rpm_R.append(pid_response.curr_rpm)
+    global desr_rpm, curr_rpm_R, curr_rpm_L, i, flag
+    curr_rpm_R.append(pid_response.motor_R)
+    curr_rpm_L.append(pid_response.motor_L)
+    if pid_response.motor_L != 0:
+        flag = True
+    else:
+        flag = False
+    err.append(pid_response.motor_R - pid_response.motor_L)
     i += 1
 
 def end():
-    global rpm_R, desr_rpm, Kp, Kd, Ki, alpha, i
-    title = "/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/rpm_tuning/on_floor_in_bcr/duty_is_pid_4_wheel/forward_desr_rpm"+str(desr_rpm)+"Kp"+str(Kp)+"_Kd"+str(Kd)+"_Ki"+str(Ki)+".png"
+    global Kp, Kd, Ki, curr_rpm_R, curr_rpm_L, i
+    title = "/home/swapnil/avitra_ws/src/auto_nav/observations_for_analysis/plots/rpm_tuning/on_floor_in_bcr/duty_is_pid_4_wheel_dd_plot/desr_rpm50_Kp"+str(Kp)+"_Kd"+str(Kd)+"_Ki"+str(Ki)+".png"
     Kp, Kd, Ki = 0, 0, 0
     publish_tuna()
-    # plt.savefig(title)
+    plt.savefig(title)
     plt.show()
     print "DONE"
     exit()
@@ -114,14 +109,17 @@ if __name__=="__main__":
     rospy.init_node('rpm_tuning',anonymous=False)
     settings = termios.tcgetattr(sys.stdin)
     pub = rospy.Publisher('tuning', tuning_msg, queue_size=10)          # publisher for teleop_key
-    rospy.Subscriber("pid_response", pid_response , pid_callback)
+    # rospy.Subscriber("pid_response", pid_response , pid_callback)
+    rospy.Subscriber("pid_response", velocity_msg, pid_callback)
     tuna = tuning_msg()    
     fig = plt.figure()
     
-    ax1 = fig.add_subplot(1,2,1)
-    ax2 = fig.add_subplot(1,2,2)
-    ax1.set_title('MOTOR_L')
-    ax2.set_title('MOTOR_R')
+    ax1 = fig.add_subplot(1,3,1)
+    ax2 = fig.add_subplot(1,3,2)
+    ax3 = fig.add_subplot(1,3,3)
+    ax1.set_title('MOTOR_L rpm')
+    ax2.set_title('MOTOR_R rpm')
+    ax3.set_title('err')
 
     ani = animation.FuncAnimation(fig, animate, interval = 1)
     fig.show()        
