d5d27b49cf768e6bbb424e6bf90fc8cb8554f0df
shweta2608 <shwetakum98@gmail.com>
2020-01-22 16:11:43 +0530

    Added pygame to ros_tran

--- src/auto_nav/scripts/old_ros_tran.py
+++ src/auto_nav/scripts/old_ros_tran.py
@@ -8,8 +8,7 @@
 # PRESS 'd' TO TURN RIGHT ON SPOT
 # PRESS 'y' TO INCREMENT SPEED BY 1
 # PRESS 'h' TO DECREMENT SPEED BY 1
-# PRESS 'z' TO START AUTONAV
-# PRESS 'esc' TO EXIT
+# PRESS 'x' TO EXIT
 ###################################################################################################################
 
 import struct
@@ -17,13 +16,6 @@ import rospy
 from std_msgs.msg import String
 import sys, select, termios, tty
 from auto_nav.msg import velocity_msg
-import pygame
-import sys
-
-def initialize_pygame():
-    pygame.init()
-    screen = pygame.display.set_mode((400, 300))
-    pygame.key.set_repeat(10,50)
 
 def deploy_payloads(direction, speed):
     global motor_F, motor_B, motor_L, motor_R, velocity
@@ -58,11 +50,20 @@ def deploy_payloads(direction, speed):
     velocity.motor_R = motor_R
     pub_velocity.publish(velocity)
     
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)                                         # read key pressed on keyboard
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
 if __name__=="__main__":
     rospy.init_node('ros_tran',anonymous=False)
+    settings = termios.tcgetattr(sys.stdin)
     pub_velocity = rospy.Publisher('teleop', velocity_msg, queue_size=10)          # publisher for teleop_key
-    rate=rospy.Rate(500)
-    initialize_pygame()
     velocity = velocity_msg()
     deploy_payloads("r", 0)
     direction = "r"
@@ -71,50 +72,34 @@ if __name__=="__main__":
     motor_B = 0
     motor_L = 0
     motor_R = 0
-    change_flag = True
     while not rospy.is_shutdown():
-        print "\ndirection =", direction, "\tspeed =", speed, "\tcommand =", velocity.command
-        pressed = pygame.key.get_pressed()
-        w_held = pressed[pygame.K_w] or pressed[pygame.K_w]
-        a_held = pressed[pygame.K_a] or pressed[pygame.K_a]
-        s_held = pressed[pygame.K_s] or pressed[pygame.K_s]
-        d_held = pressed[pygame.K_d] or pressed[pygame.K_d]
-        z_held = pressed[pygame.K_z] or pressed[pygame.K_z]
-        if w_held:
-            direction = "w"
-        elif a_held:
-            direction = "a"
-        elif s_held:
-            direction = "s"
-        elif d_held:
-            direction = "d"
-        elif z_held:
-            velocity.command = 1
-        else:
+        print "direction =", direction, "\tspeed =", speed, "\tcommand =", velocity.command
+        key = getKey()
+        if key == "r":
             direction = "r"
-            deploy_payloads("r", 0)
+        elif key == "w":
+            direction = "w"
+        elif key == "s":
+            direction = "s"
+        elif key == "a":
+            direction = "a"
+        elif key == "d":
+            direction = "d"
+        elif key == "y":
+            speed += 1
+            if speed == 1:
+                speed = 16      # little less than the starting speed
+            if speed > 30:
+                speed = 30
+        elif key == "h":
+            speed -= 1
+            if speed < 0:
+                speed = 0
+        elif key == "q":
             velocity.command = 0
-        for event in pygame.event.get():            
-            if event.type == pygame.KEYDOWN:
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.key == pygame.K_ESCAPE:
-                    sys.exit()
-                if event.key == pygame.K_y and change_flag:
-                    speed += 1
-                    if speed == 1:
-                        speed = 16      # little less than the starting speed
-                    if speed > 30:
-                        speed = 30
-                elif event.key == pygame.K_h and change_flag:
-                    speed -= 1
-                    if speed < 0:
-                        speed = 0
-                if z_held:
-                    velocity.command = 1
-                if change_flag:
-                    change_flag = False
-            if event.type == pygame.KEYUP:
-                change_flag = True
-            deploy_payloads(direction, speed)
-        rate.sleep()
+        elif key == "z":
+            velocity.command = 1
+        elif key == "x":
+            deploy_payloads("r", 0)
+            exit()
+        deploy_payloads(direction, speed)
