f8fa6952583db3ac8c70dd4b81869f3e3a1cc4be
shweta2608 <shwetakum98@gmail.com>
2020-01-28 16:09:47 +0530

    Added odometry with BNO code and new serial node launch file

--- src/auto_nav/scripts/odometry_with_bno.py
+++ src/auto_nav/scripts/odometry_with_bno.py
@@ -15,16 +15,18 @@ from auto_nav.msg import ticks_msg
 from std_msgs.msg import Float32
 
 #All in MKS
-wheel_dia  = 0.154                                                                   # wheel diameter in m
-ppr = 540   #135                                                                   # encoders ticks per rotation
+wheel_dia = 0.154
+ppr = 540  # 135*4
 bot_dia = 0.475
 temp_distance = 0.00
-angle = 0
-initial_angle = 0
-flag = False
+angle_enc = 0
+angle_imu = 0
+initial_angle_imu = 0
+imu_flag = False
 ##################################################################################################################
 
-class Queue:                                                                # queue for storing published raw ticks of the encoders
+
+class Queue:
     def __init__(self):
         self.items = []
 
@@ -39,35 +41,35 @@ class Queue:                                                                # qu
 
 ####################################################################################################################
 
+
 def ticks_callback(ticks_array):
     queue_left.enqueue(ticks_array.motor_L)
     queue_right.enqueue(ticks_array.motor_R)
-    
 
 
 def yaw_callback(yaw):
-    global angle, initial_angle, flag
-    if flag == False:
-        initial_angle = yaw.data
-        flag = True
-    else:
-        angle = yaw.data
-
+    global angle_imu #, imu_flag,initial_angle_imu
+    # if imu_flag == True:
+    #     angle_imu = initial_angle_imu - yaw.data
+    # else:
+    #    initial_angle_imu = yaw.data
+    #     angle = yaw.data
+    #     imu_flag = True
+    angle_imu = yaw.data
 
 
 #####################################################################################################################
 if __name__ == '__main__':
-    global angle,initial_angle
-    final_angle = 0
-    enc_angle = 0
-    x = 0
-    y = 0
+    angle_enc = 0
+    angle_imu = 0
+    x_enc, y_enc = 0, 0
+    x_imu, y_imu = 0, 0
     theta = 0
-    queue_right = Queue()                                                   # queue objects for right and left encoders
+    queue_right = Queue()
     queue_left = Queue()
-    rospy.init_node('avitra_odometry', anonymous=False)
-    rospy.Subscriber("ticks", ticks_msg, ticks_callback)                # Subscribing to raw encoder tics and velocities
-    rospy.Subscriber("yaw",Float32, yaw_callback)
+    rospy.init_node('avitra_odometry', anonymous=True)
+    rospy.Subscriber("ticks", ticks_msg, ticks_callback)
+    rospy.Subscriber("yaw", Float32, yaw_callback)
     odom_broadcaster = tf.TransformBroadcaster()
     laser_broadcaster = tf.TransformBroadcaster()
     while not rospy.is_shutdown():
@@ -75,34 +77,21 @@ if __name__ == '__main__':
             if not(queue_left.is_empty()):
                 ticksR = queue_right.dequeue()
                 ticksL = queue_left.dequeue()
-                enc_theta =(ticksR-ticksL)*pi*wheel_dia/(bot_dia*ppr)                    # calculation of angular displacement from latest orientation
-                enc_angle = enc_angle + enc_theta                                       # absolute angular displacement
+                theta = (ticksR-ticksL)*pi*wheel_dia/(bot_dia*ppr)
+                angle_enc = angle_enc + theta
+                temp_distance = ((ticksL+ticksR)/2)*pi*wheel_dia/ppr
+                x_enc = x_enc + temp_distance*cos(angle_enc)
+                y_enc = y_enc + temp_distance*sin(angle_enc)
+                x_imu = x_imu + temp_distance*cos(radians(angle_imu))
+                y_imu = y_imu + temp_distance*sin(radians(angle_imu))
+                print "ENCODER: angle = " + str(degrees(angle_enc)), "x = ", str(x_enc), "y = ", str(y_enc), "IMU: angle = " + str(angle_imu), "x = ", str(x_imu), "y = ", str(y_imu)
 
-                        
-                theta = angle - initial_angle
-                
-                if ((360 - initial_angle) < initial_angle) and (angle < (360 - angle)):
-                    final_angle -= 360 + theta
-                elif ((360 - initial_angle) > initial_angle) and (angle > (360 - angle)):
-                    final_angle += 360 - theta
-                else:
-                    final_angle -= theta
-                print "Final angle = " + str(final_angle) #+ "\tInitial angle = " + str(initial_angle) + "\t angle = " + str(angle) 
-                initial_angle = angle
-
-                temp_distance = ((ticksL+ticksR)/2)*pi*wheel_dia/ppr           # local linear displacemnt (from previous position)
-                x = x + temp_distance*cos(final_angle)                            # absolute postions w.r.t origin
-                y = y + temp_distance*sin(final_angle)
-
-            odom_quat = tf.transformations.quaternion_from_euler(0, 0, angle)
-            odom_broadcaster.sendTransform(                                                                                 # transformation of robot base_link as computed from odometry data
-            (x, y, 0.),
-            odom_quat,
-            rospy.Time.now(),
-            "base_link",
-            "odom"
-            )
-            laser_broadcaster.sendTransform((0, 0, 0.25), (0, 0, 0, 1), rospy.Time.now(), "camera_depth_frame", "base_link")       # fixed transform between robot base and laser scanner
+            odom_quat = tf.transformations.quaternion_from_euler(
+                0, 0, angle_enc)
+            odom_broadcaster.sendTransform(
+                (x_enc, y_enc, 0.), odom_quat, rospy.Time.now(), "base_link", "odom")
+            laser_broadcaster.sendTransform(
+                (0, 0, 0.25), (0, 0, 0, 1), rospy.Time.now(), "camera_depth_frame", "base_link")
         except Exception as E:
             print "EXCEPTION", E
             continue
