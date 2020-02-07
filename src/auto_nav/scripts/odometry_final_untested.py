a980a33117bc903f93ce87fbec6b2c7e2bd622f5
shweta2608 <shwetakum98@gmail.com>
2020-02-07 16:20:49 +0530

    Integrated camera part in odom code

--- src/auto_nav/scripts/odometry_final_untested.py
+++ src/auto_nav/scripts/odometry_final_untested.py
@@ -13,6 +13,7 @@ import tf
 from nav_msgs.msg import Odometry
 from auto_nav.msg import ticks_msg
 from perception.msg import array,array_float
+from auto_nav.msg import velocity_msg
 
 #All in MKS
 wheel_dia  = 0.154                                                                   # wheel diameter in m
@@ -20,6 +21,8 @@ ppr = 540   #135                                                                
 bot_dia = 0.475
 temp_distance = 0.00
 angle = 0
+rpmL = 0
+rpmR = 0
 obj_x = 0
 obj_y = 0
 flag = False
@@ -49,6 +52,12 @@ def ticks_callback(ticks_array):
     queue_left.enqueue(ticks_array.motor_L)
     queue_right.enqueue(ticks_array.motor_R)
 
+
+def rpm_callback(rpm):
+    global rpmL, rpmR
+    rpmL = rpm.motor_L
+    rpmR = rpm.motor_R
+
 def camera_coordinates(cam_array):
     global flag, x, y, obj_x, obj_y, theta
     cam_z = cam_array.array[2]
@@ -58,24 +67,19 @@ def camera_coordinates(cam_array):
         obj_x = x + cam_z*cos(theta) + offset_x
         obj_y = y + cam_x*sin(theta) + offset_y
         flag = True
-    else:
-        obj_x = obj_x*count + (x + cam_z + offset_x)
-        obj_x /= (count + 1)
-        obj_y = obj_y*count + (y + cam_x + offset_y)
-        obj_y /= (count + 1)
-        count+=1
-    print(obj_x, obj_y)
+
+    print("obj_x = " + str(obj_x) + " obj_y = " + str(obj_y))
     
 #####################################################################################################################
 
 if __name__ == '__main__':
-    global x,y
-    
+    global x,y    
     angle = 0
     queue_right = Queue()                                                   # queue objects for right and left encoders
     queue_left = Queue()
     rospy.init_node('avitra_odometry', anonymous=False)
     rospy.Subscriber("ticks", ticks_msg, ticks_callback)                # Subscribing to raw encoder tics and velocities
+    rospy.Subscriber("teleop", velocity_msg, rpm_callback)
     rospy.Subscriber("position" ,array_float ,camera_coordinates)
     odom_broadcaster = tf.TransformBroadcaster()
     laser_broadcaster = tf.TransformBroadcaster()
@@ -86,10 +90,13 @@ if __name__ == '__main__':
                 ticksL = queue_left.dequeue()
                 theta =(ticksR-ticksL)*pi*wheel_dia/(bot_dia*ppr)                    # calculation of angular displacement from latest orientation
                 angle = angle + theta                                       # absolute angular displacement
-                temp_distance = ((ticksL+ticksR)/2)*pi*wheel_dia/ppr           # local linear displacemnt (from previous position)
+                if rpmL == rpmR and rpmL != 0:
+                    temp_distance = 0
+                else:    
+                    temp_distance = ((ticksL+ticksR)/2)*pi*wheel_dia/ppr           # local linear displacemnt (from previous position)
                 x = x + temp_distance*cos(angle)                            # absolute postions w.r.t origin
                 y = y + temp_distance*sin(angle)
-
+                print "Angle = " + str((angle*180/3.142)%360) + "\tX = " + str(x) + "\tY = " + str(y)
             odom_quat = tf.transformations.quaternion_from_euler(0, 0, angle)
             odom_broadcaster.sendTransform(                                                                                 # transformation of robot base_link as computed from odometry data
             (x, y, 0.),
